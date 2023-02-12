2225fb98aa1f3b7057136ac5aa753d57398d930e
fierysoul <ivmorozov001@gmail.com>
2023-02-12 19:56:01 +0300

    Добавлен тестовый пайплайн

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,17 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
